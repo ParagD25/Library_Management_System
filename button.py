9cf40b3c1a0b142c728bd3efe30b9d33dc4ea82b
Parag <paragdurafe@outlook.com>
2021-07-04 21:18:24 +0530

    Optimized

--- button.py
+++ button.py
@@ -1,47 +1,34 @@
 import sqlite3
-def connect_to_database():
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('create table if not exists book (id INTEGER PRIMARY KEY,title TEXT,author TEXT,year INTEGER,isbn INTEGER)')
-    conn.commit()
-    conn.close() 
+class Data():
+    def __init__(self):
+        self.conn=sqlite3.connect('lib_books.db')
+        self.cur=self.conn.cursor()
+        self.cur.execute('create table if not exists book (id INTEGER PRIMARY KEY,title TEXT,author TEXT,year INTEGER,isbn INTEGER)')
+        self.conn.commit()
 
-def insert_in_database(title,author,year,isbn):
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('insert into book values (null,?,?,?,?)',(title,author,year,isbn))
-    conn.commit()
-    conn.close() 
-
-def view_data():
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('select * from book')
-    view=cur.fetchall()
-    conn.close() 
-    return view
-
-def search_in_database(title='',author='',year='',isbn=''):
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('select * from book where title=? or author=? or year=? or isbn=?',(title,author,year,isbn))
-    search=cur.fetchall()
-    conn.close() 
-    return search  
-
-def update_record(id,title,author,year,isbn):
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('update book set title=?,author=?,year=?,isbn=? where id=?',(title,author,year,isbn,id))
-    conn.commit()
-    conn.close() 
-
-def delete_record(id):
-    conn=sqlite3.connect('lib_books.db')
-    cur=conn.cursor()
-    cur.execute('delete from book where id=?',(id,))
-    conn.commit()
-    conn.close() 
+    def insert_in_database(self,title,author,year,isbn):
+        self.cur.execute('insert into book values (null,?,?,?,?)',(title,author,year,isbn))
+        self.conn.commit()
 
 
-connect_to_database()
+    def view_data(self):
+        self.cur.execute('select * from book')
+        view=self.cur.fetchall()
+        return view
+
+    def search_in_database(self,title='',author='',year='',isbn=''):
+
+        self.cur.execute('select * from book where title=? or author=? or year=? or isbn=?',(title,author,year,isbn))
+        search=self.cur.fetchall()
+        return search  
+
+    def update_record(self,id,title,author,year,isbn):
+        self.cur.execute('update book set title=?,author=?,year=?,isbn=? where id=?',(title,author,year,isbn,id))
+        self.conn.commit()
+
+    def delete_record(self,id):
+        self.cur.execute('delete from book where id=?',(id,))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
